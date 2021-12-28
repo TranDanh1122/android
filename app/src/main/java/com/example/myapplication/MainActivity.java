793d3f085d81341586a5b92f27f412d958c6984a
TranDanh1122 <trandanh14042000@gmail.com>
2021-12-28 16:06:02 +0700

    update login logout

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button dangnhap,dangki;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         dangnhap=findViewById(R.id.chuyenlogin);
+         dangki=findViewById(R.id.chuyenregister);
+         dangki.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 startActivity(new Intent(MainActivity.this,DangkyActivity.class));
+                 finish();
+             }
+         });
+         dangnhap.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 startActivity(new Intent(MainActivity.this,DangnhapActivity.class));
+                 finish();
+             }
+         });
+    }
+}
